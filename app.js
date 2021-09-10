f870f85e66d81447b3eed1d8da9babee6dc01013
namanhishere <namanhishere0208@gmail.com>
2021-09-10 08:39:33 +0700

    update proxy

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('trust proxy', 1);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
